df5d4ebd2a5dfba2f0dcefe7d8220dcb32c67c87
sowinskipatryk <91700001+sowinskipatryk@users.noreply.github.com>
2023-05-07 13:33:34 +0200

    temporary coloring

--- src/components/Seat.js
+++ src/components/Seat.js
@@ -3,6 +3,7 @@ import { modalActions } from "../store/index";
 import { useDispatch } from "react-redux";
 import Modal from "./UI/Modal";
 import { useSelector } from "react-redux";
+import convert from 'color-convert';
 
 const DUMMY_DATA = Array(106).fill().map(() => Math.random().toFixed(2));
 
@@ -11,6 +12,10 @@ const Seat = (props) => {
   const dispatch = useDispatch();
   const modalOpen = useSelector(state => state.modalOpen);
   const id = props.id;
+  const dataValue = DUMMY_DATA[id-1];
+  const [hue, saturation, value] = [dataValue * 120, 90, 80];
+  const [rVal, gVal, bVal] = convert.hsv.rgb(hue, saturation, value);
+  const rgbValue = `rgb(${rVal}, ${gVal}, ${bVal})`;
 
   const handleOpenModal = () => {
     dispatch(modalActions.openModal(id));
@@ -25,10 +30,11 @@ const Seat = (props) => {
       className={`${styles.seat} ${styles["seat-" + props.direction]}`}
       onMouseEnter={handleOpenModal}
       onMouseLeave={handleCloseModal}
+      style={{backgroundColor: rgbValue}}
     >
       {showNumbers ? props.id : null}
     </div>
-    {modalOpen === id ? <Modal text={DUMMY_DATA[id-1]} /> : null}
+    {modalOpen === id ? <Modal text={dataValue} /> : null}
     </div>
   );
 };
